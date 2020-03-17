ec8eda8f0ec7a63716865685c012e4fb4d772091
KristenBrandt <kabrandtf@gmail.com>
2020-03-16 21:10:38 -0600

    inicio

---
+++ login.py
@@ -0,0 +1,202 @@
+from tkinter import *
+import os
+
+def main_account_screen():
+
+    global main_screen
+    main_screen = Tk()   # create a GUI window
+    main_screen.geometry("2000x1500") # set the configuration of GUI window
+    main_screen.title("Account Login") # set the title of GUI window
+    main_screen.configure(background = 'black')
+
+    ## CREAR IMAGEN
+    foto = PhotoImage(file = "header.png")
+    label2 = Label(main_screen, image = foto)
+    label2.place(x=90, y =0)
+
+    # create a Form label
+    Label(text="Login Or Register", bg="blue", width="160", height="2", fg='white',font=("Calibri", 13)).place(y=260)
+
+    # create Login Button
+    Button(text="Login", height="4", width="60",highlightbackground='magenta', fg='black',command = login).place(x=460, y=400)
+
+    # create a register button
+    Button(text="Register", bg='black', height="4", width="60", fg= 'black',highlightbackground='magenta', command = register).place(x=460,y=600)
+
+    main_screen.mainloop() # start the GUI
+
+def register():
+    global register_screen
+    global username
+    global password
+    global username_entry
+    global password_entry
+
+    # The Toplevel widget work pretty much like Frame,
+    # but it is displayed in a separate, top-level window.
+    #Such windows usually have title bars, borders, and other “window decorations”.
+    # And in argument we have to pass global screen variable
+
+    register_screen = Toplevel(main_screen)
+    register_screen.title("Register")
+    register_screen.geometry("2000x1500")
+    register_screen.configure(background = 'black')
+
+    ## CREAR IMAGEN
+    foto2 = PhotoImage(file = "header.png")
+    label2 = Label(register_screen, image = foto2)
+    label2.image =foto2
+    label2.place(x=90, y =0)
+
+
+    # Set text variables
+    username = StringVar()
+    password = StringVar()
+
+    # Set label for user's instruction
+    Label(register_screen, text="Please enter details below", bg="blue", fg='white',width="160", height="2").place(x=5,y=260)
+
+    # Set username label
+    username_lable3 = Label(register_screen, text="Username * ")
+    username_lable3.place(x=675,y=320)
+
+    # Set username entry
+    # The Entry widget is a standard Tkinter widget used to enter or display a single line of text.
+
+    username_entry = Entry(register_screen, textvariable=username)
+    username_entry.place(x=635,y=345)
+
+    # Set password label
+    password_lable4 = Label(register_screen, text="Password * ")
+    password_lable4.place(x=675,y=385)
+
+    # Set password entry
+    password_entry = Entry(register_screen, textvariable=password, show='*')
+    password_entry.place(x=635,y=410)
+
+
+
+    # Set register button
+    Button(register_screen, text="Register", width=10, height=2, bg="blue", command =register_user).place(x=675,y=470)
+
+
+def register_user():
+
+    # get username and password
+    username_info = username.get()
+    password_info = password.get()
+
+    # Open file in write mode
+    file = open(username_info, "w")
+
+    # write username and password information into file
+    file.write(username_info + "\n")
+    file.write(password_info)
+    file.close()
+
+    username_entry.delete(0, END)
+    password_entry.delete(0, END)
+
+    # set a label for showing success information on screen
+    Label(register_screen, text="Registration Success", fg="white",bg='black', font=("calibri", 11)).place(x=675,y=600)
+
+# define login function
+def login():
+    global login_screen
+    login_screen = Toplevel(main_screen)
+    login_screen.title("Login")
+    login_screen.geometry("2000x1500")
+    login_screen.configure(background = 'black')
+
+    ## CREAR IMAGEN
+    foto3 = PhotoImage(file = "header.png")
+    label3 = Label(login_screen, image = foto3)
+    label3.image =foto3
+    label3.place(x=90, y =0)
+
+    Label(login_screen, text="Please enter details below to login",width="160", height="2", bg="blue", fg='white').place(x=5,y=260)
+    global username_login_entry
+    global password__login_entry
+    global username_verify
+    global password_verify
+
+    username_verify = StringVar()
+    password_verify = StringVar()
+
+
+    Label(login_screen, text="Username * ").place(x=675,y=320)
+    username_login_entry = Entry(login_screen, textvariable=username_verify)
+    username_login_entry.place(x=635,y=345)
+    Label(login_screen, text="Password * ").place(x=675,y=385)
+    password__login_entry = Entry(login_screen, textvariable=password_verify, show= '*')
+    password__login_entry.place(x=635,y=410)
+
+    Button(login_screen, text="Login", width=10, height=1, command=login_verify).place(x=675,y=470)
+
+def login_verification():
+    print("working...")
+
+def login_verify():
+    #get username and password
+
+    username1 = username_verify.get()
+    password1 = password_verify.get()
+    # this will delete the entry after login button is pressed
+    username_login_entry.delete(0, END)
+    password__login_entry.delete(0, END)
+
+    #The method listdir() returns a list containing the names of the entries in the directory given by path.
+    list_of_files = os.listdir()
+
+    #defining verification's conditions
+    if username1 in list_of_files:
+        file1 = open(username1, "r")   # open the file in read mode
+
+        #read the file,
+        #as splitlines() actually splits on the newline character,
+        #the newline character is not left hanging at the end of each line. if password1 in verify:
+
+        verify = file1.read().splitlines()
+        login_sucess()
+
+    else:
+        password_not_recognised()
+
+
+def login_sucess():
+    global login_success_screen   # make login_success_screen global
+    login_success_screen = Toplevel(login_screen)
+    login_success_screen.title("Success")
+    login_success_screen.geometry("150x100")
+    Label(login_success_screen, text="Login Success").pack()
+
+    # create OK button
+    Button(login_success_screen, text="OK", command=delete_login_success).pack()
+
+
+def delete_login_success():
+    login_success_screen.destroy()
+
+def password_not_recognised():
+    global password_not_recog_screen
+    password_not_recog_screen = Toplevel(login_screen)
+    password_not_recog_screen.title("Success")
+    password_not_recog_screen.geometry("150x100")
+    Label(password_not_recog_screen, text="Invalid Password ").pack()
+    Button(password_not_recog_screen, text="OK", command=delete_password_not_recognised).pack()
+
+def delete_password_not_recognised():
+    password_not_recog_screen.destroy()
+
+def user_not_found():
+    global user_not_found_screen
+    user_not_found_screen = Toplevel(login_screen)
+    user_not_found_screen.title("Success")
+    user_not_found_screen.geometry("150x100")
+    Label(user_not_found_screen, text="User Not Found").pack()
+    Button(user_not_found_screen, text="OK", command=delete_user_not_found_screen).pack()
+
+def delete_user_not_found_screen():
+    user_not_found_screen.destroy()
+
+main_account_screen() # call the main_account_screen() function
